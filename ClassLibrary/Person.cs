aa6c1c604651bc23f0d76e0b6af10f13c711d8d2
kushal <collabskus@gmail.com>
2025-09-14 18:49:01 -0700

    add some basic code

---
+++ ClassLibrary/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary;
+
+public class Person(string FirstName, string LastName)
+{
+    public string GetLegalName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassLibrary;
+Console.WriteLine("Please enter the first name");
+string? firstName = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(firstName))
+{
+    // ask again 
+    Console.WriteLine("Please enter the first name");
+    firstName = Console.ReadLine();
+}
+Console.WriteLine("Please enter the last name");
+string? lastName = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(lastName))
+{
+    // ask again 
+    Console.WriteLine("Please enter the last name");
+    lastName = Console.ReadLine();
+}
+Person person = new(firstName, lastName);
+Console.WriteLine($"Hello, {person.GetLegalName()}!");
